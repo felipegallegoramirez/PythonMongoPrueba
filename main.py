4f38dd6b5d343c3f3f53f1db26e8e1abcb717d60
Felipe Gallego <felipegallego600@gmail.com>
2023-06-21 15:52:56 -0500

    find and create

--- main.py
+++ main.py
@@ -4,14 +4,33 @@ MONGO_HOST="localhost"
 MONGO_PORT="27017"
 MONGO_TIME_OVER=1000
 
-MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PORT+"/"
+MONGO_URI="mongodb://"+MONGO_HOST+":"+MONGO_PORT+""
+
+
+
+#Coleccion?
+MONGO_BD="Escuela"
+MONGO_COLLECTION="alumnos"
+
+data={
+    "nombre":"hola",
+    "Edad":20,
+}
 
 try:
     cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMs=MONGO_TIME_OVER)
-    cliente.server_info()
+    bd=cliente[MONGO_BD]
+    collection=bd[MONGO_COLLECTION]
+    collection.insert_one(data)
+    #cliente.server_info()
+    for s in collection.find():
+        print(s)
+
+
+
     print("Exitoso")
     cliente.close()
 except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
     print("Time extend: "+ errorTiempo)
-except pymongo.errors.ConectionFailure as errorConexion:
+except pymongo.errors.ConnectionFailure as errorConexion:
     print("Time extend: "+ errorConexion)
